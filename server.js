3e00a6f4972e76e290b1e00c03b903e1b7d82857
andriymurovanyi <andriymurovanyi@gmail.com>
2019-12-11 23:01:25 +0200

    Merge branch 'master' of https://github.com/dean-developers/pizza-panel
    # Conflicts:
    #	server.js

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const express = require('express');
-const serveStatic = require('serve-static');
+const Koa = require('koa')
+const cors = require('@koa/cors')
+const serve = require('koa-static')
+const body = require('koa-body')
+const views = require('koa-views')
+const Router = require('koa-router')
+const path = require('path')
 
-let app = express();
-app.use(serveStatic(__dirname + "/dist"));
+const app = new Koa()
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log('Listening on port ' + port)
-});
+app.use(cors())
+app.use(body())
+
+app.use(views(path.join(__dirname, './dist')))
+app.use(serve('./dist'))
+
+const router = new Router()
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+
+router.get('/*', async function(ctx) {
+    return ctx.render('index')
+})
+
+if (!module.parent) {
+    app.listen(process.env.PORT || 3000)
+}
