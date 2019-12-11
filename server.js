8ffde92043455bd2d2a6cab55595c90c9176499a
denissemo <denissemo1999@gmail.com>
2019-12-11 22:26:12 +0200

    fix server

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const serveStatic = require('serve-static');
 let app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
+app.get('/*', function(req, res) {
+	res.render('index');
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log('Listening on port ' + port)
